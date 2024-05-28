775aa3c8586bd203483125e1c5176e016dfb3d37
System.out.print ("Runance") <rebeccaaguilar047@gmail.com>
2024-05-28 17:05:44 +0800

    ggffgg

---
+++ CafeCheckOutDisplayingQueues/CafeCheckOutDisplayingQueues/Queqe Display.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace CafeCheckOutDisplayingQueues
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label labelToGoFirst;
+        private System.Windows.Forms.Label labelToGoNext;
+        private System.Windows.Forms.Label labelForHereFirst;
+        private System.Windows.Forms.Label labelForHereNext;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelToGoFirst = new System.Windows.Forms.Label();
+            this.labelToGoNext = new System.Windows.Forms.Label();
+            this.labelForHereFirst = new System.Windows.Forms.Label();
+            this.labelForHereNext = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelToGoFirst
+            // 
+            this.labelToGoFirst.AutoSize = true;
+            this.labelToGoFirst.BackColor = System.Drawing.Color.Transparent;
+            this.labelToGoFirst.Font = new System.Drawing.Font("Rockwell", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelToGoFirst.Location = new System.Drawing.Point(600, 310);
+            this.labelToGoFirst.Name = "labelToGoFirst";
+            this.labelToGoFirst.Size = new System.Drawing.Size(264, 38);
+            this.labelToGoFirst.TabIndex = 0;
+            this.labelToGoFirst.Text = "First Customer: ";
+            // 
+            // labelToGoNext
+            // 
+            this.labelToGoNext.AutoSize = true;
+            this.labelToGoNext.BackColor = System.Drawing.Color.Transparent;
+            this.labelToGoNext.Font = new System.Drawing.Font("Rockwell", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelToGoNext.Location = new System.Drawing.Point(600, 352);
+            this.labelToGoNext.Name = "labelToGoNext";
+            this.labelToGoNext.Size = new System.Drawing.Size(268, 38);
+            this.labelToGoNext.TabIndex = 1;
+            this.labelToGoNext.Text = "Next Customer: ";
+            // 
+            // labelForHereFirst
+            // 
+            this.labelForHereFirst.AutoSize = true;
+            this.labelForHereFirst.BackColor = System.Drawing.Color.Transparent;
+            this.labelForHereFirst.Font = new System.Drawing.Font("Rockwell", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelForHereFirst.Location = new System.Drawing.Point(153, 310);
+            this.labelForHereFirst.Name = "labelForHereFirst";
+            this.labelForHereFirst.Size = new System.Drawing.Size(264, 38);
+            this.labelForHereFirst.TabIndex = 2;
+            this.labelForHereFirst.Text = "First Customer: ";
+            // 
+            // labelForHereNext
+            // 
+            this.labelForHereNext.AutoSize = true;
+            this.labelForHereNext.BackColor = System.Drawing.Color.Transparent;
+            this.labelForHereNext.Font = new System.Drawing.Font("Rockwell", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelForHereNext.Location = new System.Drawing.Point(153, 352);
+            this.labelForHereNext.Name = "labelForHereNext";
+            this.labelForHereNext.Size = new System.Drawing.Size(268, 38);
+            this.labelForHereNext.TabIndex = 3;
+            this.labelForHereNext.Text = "Next Customer: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(155, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Entry Type For Here";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(602, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Entry Type To Go";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(318, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(502, 72);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "\"CafeCheckOut\"";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Rockwell", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(290, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(554, 72);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Costumer Queue\'s";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(193, 492);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(722, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Please Wait Patiently Your Order Has Beenn Processed!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1117, 641);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelForHereNext);
+            this.Controls.Add(this.labelForHereFirst);
+            this.Controls.Add(this.labelToGoNext);
+            this.Controls.Add(this.labelToGoFirst);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Queue Display";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ CafeCheckOutDisplayingQueues/CafeCheckOutDisplayingQueues/Queqe Display.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace CafeCheckOutDisplayingQueues
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Data Source=LAPTOP-R45B7D8N\SQLEXPRESS;Initial Catalog=Cafedatabase;Integrated Security=True;";
+        private System.Windows.Forms.Timer updateTimer;
+        private System.Windows.Forms.Timer blinkTimer;
+        private bool isBlinkingVisible = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeUpdateTimer();
+            InitializeBlinkTimer();
+            LoadQueueData();
+        }
+
+        private void InitializeUpdateTimer()
+        {
+            updateTimer = new System.Windows.Forms.Timer();
+            updateTimer.Interval = 10000; // Set the interval to 10 seconds for updating data source
+            updateTimer.Tick += UpdateTimer_Tick;
+            updateTimer.Start();
+        }
+
+        private void InitializeBlinkTimer()
+        {
+            blinkTimer = new System.Windows.Forms.Timer();
+            blinkTimer.Interval = 5500; // Interval for the blinking effect
+            blinkTimer.Tick += BlinkTimer_Tick;
+            blinkTimer.Start();
+        }
+
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            LoadQueueData();
+        }
+
+        private void BlinkTimer_Tick(object sender, EventArgs e)
+        {
+            isBlinkingVisible = !isBlinkingVisible;
+            ToggleLabelVisibility(isBlinkingVisible);
+        }
+
+        private void ToggleLabelVisibility(bool isVisible)
+        {
+            labelToGoFirst.Visible = isVisible;
+            labelToGoNext.Visible = isVisible;
+            labelForHereFirst.Visible = isVisible;
+            labelForHereNext.Visible = isVisible;
+        }
+
+        private void LoadQueueData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Fetch "To Go" data
+                string toGoQuery = @"SELECT c.Queuing_num 
+                                     FROM Customer c 
+                                     JOIN Transactions t ON c.Customer_Id = t.Customer_Id 
+                                     WHERE c.Entry_type = 'To Go' 
+                                     AND t.Status = 'Queued' 
+                                     AND c.Entry_type != 'Cancelled' 
+                                     ORDER BY c.Queuing_num";
+
+                SqlDataAdapter toGoAdapter = new SqlDataAdapter(toGoQuery, connection);
+                DataTable toGoTable = new DataTable();
+                toGoAdapter.Fill(toGoTable);
+
+                // Fetch "For Here" data
+                string forHereQuery = @"SELECT c.Queuing_num 
+                                        FROM Customer c 
+                                        JOIN Transactions t ON c.Customer_Id = t.Customer_Id 
+                                        WHERE c.Entry_type = 'Here' 
+                                        AND t.Status = 'Queued' 
+                                        AND c.Entry_type != 'Cancelled' 
+                                        ORDER BY c.Queuing_num";
+
+                SqlDataAdapter forHereAdapter = new SqlDataAdapter(forHereQuery, connection);
+                DataTable forHereTable = new DataTable();
+                forHereAdapter.Fill(forHereTable);
+
+                UpdateLabels(toGoTable, forHereTable);
+            }
+        }
+
+        private void UpdateLabels(DataTable toGoTable, DataTable forHereTable)
+        {
+            labelToGoFirst.Text = toGoTable.Rows.Count > 0 ? "Current Customer: " + toGoTable.Rows[0]["Queuing_num"].ToString() : "Current Customer: None";
+            labelToGoNext.Text = toGoTable.Rows.Count > 1 ? "Next Customer: " + toGoTable.Rows[1]["Queuing_num"].ToString() : "Next Customer: None";
+
+            labelForHereFirst.Text = forHereTable.Rows.Count > 0 ? "Current Customer: " + forHereTable.Rows[0]["Queuing_num"].ToString() : "Current Customer: None";
+            labelForHereNext.Text = forHereTable.Rows.Count > 1 ? "Next Customer: " + forHereTable.Rows[1]["Queuing_num"].ToString() : "Next Customer: None";
+        }
+    }
+}
